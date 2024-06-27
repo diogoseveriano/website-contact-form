93fcb7f9db7c678d670b67e36af7f9e1e203e7b7
E012130 <diogo.rodrigues.severiano@cgd.pt>
2024-06-27 14:08:15 +0100

    contact form

--- src/main/java/luis/silvestre/lda/ContactForm.java
+++ src/main/java/luis/silvestre/lda/ContactForm.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.core.Response;
 import luis.silvestre.lda.model.Contact;
 import luis.silvestre.lda.model.ContactFormRecord;
 import luis.silvestre.lda.service.ContactFormService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.annotations.Param;
 
 @Path("/contact")
@@ -14,6 +15,9 @@ public class ContactForm {
 
     @Inject
     private ContactFormService contactFormService;
+
+    @ConfigProperty(name = "ls.wc.admin-key")
+    private String adminKey;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -28,5 +32,16 @@ public class ContactForm {
     @Path("/new")
     public Response contact(@Param ContactFormRecord contactFormRecord) {
         return Response.ok(contactFormService.newContact(contactFormRecord)).build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@QueryParam("contact_id") long id, String admin) {
+        if (admin.equals(adminKey)) {
+            Contact.deleteById(id);
+            return Response.ok().build();
+        } else {
+            return Response.notModified().build();
+        }
     }
 }
